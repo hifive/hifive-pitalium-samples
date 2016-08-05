d3fd261d031eb8d73a6337a358140802d619040c
Marie Sakai <sakai.marie.6b4@jp.nssol.nssmc.com>
2016-08-05 17:24:55 +0900

    Copyrightの年を更新

--- pitalium-sample/src/main/java/com/htmlhifive/pitalium/sample/pdf/PDFReadTest.java
+++ pitalium-sample/src/main/java/com/htmlhifive/pitalium/sample/pdf/PDFReadTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015-2016 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.htmlhifive.pitalium.sample.pdf;
 
 import java.awt.image.BufferedImage;
@@ -21,8 +37,6 @@ import com.htmlhifive.pitalium.image.util.ImageUtils;
 /**
  * PitalimでPDFを比較するサンプル。 <br/>
  * PDFの変換にはApache PDFBox(https://pdfbox.apache.org/) を使用している。
- *
- * @author sakaim
  */
 public class PDFReadTest {
 
